86e9927f0f4fbc7f2647beef6781a25a801dd439
Muhammad Fahad <fahad.zahid2004@gmail.com>
2024-05-29 22:17:09 +0000

    conditonal statments in c

---
+++ Condtional_Statements/condtions.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("Getting started with condtional statements");
+    //Conditional Statements are of two types:
+    // 1). if-else 2) switch statements
+    return 0;
+}
